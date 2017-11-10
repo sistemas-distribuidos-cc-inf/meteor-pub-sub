c6f0b669b0ea37888d5bec09b05ce0ec4f141371
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2017-11-10 00:56:25 -0200

    visual

--- app/client/rota.js
+++ app/client/rota.js
@@ -3,11 +3,14 @@ Router.configure({
 });
 
 Router.route('/', function () {
+  this.layout('ApplicationLayout');  
   this.layout('main');
 });
 Router.route('/pub', function () {
+  this.layout('ApplicationLayout');  
   this.layout('pub');
 });
 Router.route('/sub', function () {
+  this.layout('ApplicationLayout');  
   this.layout('sub');
 });
